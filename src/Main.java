30dbacc0888592cb93eef674628a9d055d8d3a16
BeomBeom2 <whwjdqja20@naver.com>
2026-04-20 11:17:34 +0900

    [BE12]조정범

---
+++ src/Main.java
@@ -0,0 +1,70 @@
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        System.out.println("== 명언 앱 ==");
+        Scanner sc = new Scanner(System.in);
+        int mapIdx = 1;
+        HashMap<Integer, String[]> map = new HashMap<Integer, String[]>();
+
+        while(true) {
+            System.out.print("명령) ");
+
+            String cmd = sc.nextLine().trim();
+
+            if(cmd.equals("종료")) break;
+            else if(cmd.equals("등록")) {
+                System.out.print("명언 : ");
+                String saying = sc.nextLine().trim();
+                System.out.print("작가 : ");
+                String writer = sc.nextLine().trim();
+                map.put(mapIdx++, new String[]{saying, writer});
+                System.out.println(map.size() + "번 명언이 등록되었습니다.");
+            } else if(cmd.equals("목록")){
+                System.out.println("번호 / 작가 / 명언");
+                System.out.println("----------------------");
+                for(int i = mapIdx-1 ; i > 0; i--) {
+                    if(map.containsKey(i)){
+                        String [] cur = map.get(i);
+                        System.out.println(i + " / " + cur[1] + " / " + cur[0]);
+                    }
+                }
+            } else {
+                int delIdx = cmd.indexOf("?");
+                String tmp = "";
+                if(delIdx != -1)
+                    tmp = cmd.substring(0, delIdx);
+
+
+                if(!cmd.contains("=")) continue;
+                int idx = Integer.parseInt(cmd.substring(cmd.indexOf("=")+1));
+
+                if(tmp.equals("삭제")){
+                    if(map.get(idx) == null){
+                        System.out.println(idx + "번 명언은 존재하지 않습니다.");
+                    } else {
+                        map.remove(idx);
+                        System.out.println(idx + "번 명언이 삭제되었습니다.");
+                    }
+                } else if(tmp.equals("수정")){
+                    if(map.get(idx) == null){
+                        System.out.println(idx + "번 명언은 존재하지 않습니다.");
+                    } else {
+                        String[] cur = map.get(idx);
+                        String[] newStr = new String[2];
+                        System.out.println("명언(기존) : " + cur[0]);
+                        System.out.print("명언 : ");
+                        newStr[0] = sc.nextLine().trim();
+                        System.out.println("작가(기존) : " + cur[1]);
+                        System.out.print("작가 : ");
+                        newStr[1] = sc.nextLine().trim();
+
+                        map.put(idx, newStr);
+                    }
+                }
+            }
+        }
+    }
+}
